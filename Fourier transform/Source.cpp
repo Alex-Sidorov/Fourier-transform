0e80a581907442f1e6be611bd50e64ac00d614f1
Саша Сидоров <Sidorov1999@bk.ru>
2018-03-04 22:27:58 +0300

    compute

--- Fourier transform/Source.cpp
+++ Fourier transform/Source.cpp
@@ -2,10 +2,18 @@
 #include <math.h>
 #include <conio.h>
 
+const double PI = 3.14159265;
+
+struct Complex
+{
+	double real;
+	double image;
+};
+
 int main()
 {
-	double *x_n;
-	double *X_k;
+	Complex *x_n = nullptr;
+	Complex *X_k = nullptr;
 	int count_N = 0;
 
 	std::cout << "Input count samples(N):";
@@ -13,21 +21,34 @@ int main()
 
 	try
 	{
-		x_n = new double[count_N];
-		X_k = new double[count_N];
+		x_n = new Complex[count_N];
+		X_k = new Complex[count_N];
 	}
 	catch (std::bad_alloc)
 	{
-		std::cout << "Error alloc.Press for exit";
+		std::cout << std::endl << "Error alloc.Press for exit";
 		_getch();
+		return -1;
 	}
 
 	for (int i = 0; i < count_N; i++)
 	{
-		std::cout << "Please input value " << "x(" << i << "):";
-		std::cin >> x_n[i];
+		std::cout << "Please input value " << "x(" << i << ")" << std::endl;
+		std::cout << "Real:";
+		std::cin >> x_n[i].real;
+		std::cout << std::endl << "Image:";
+		std::cin >> x_n[i].image;
 	}
 
-
+	double summa = 0;
+	int j = sqrt(-1);
+	for (int i = 0; i < count_N; i++)
+	{
+		for (int j = 0; j < count_N; j++)
+		{
+			X_k[i].real += x_n[i].real * cos(2 * PI*i*j / count_N);
+			X_k[i].image += -1*x_n[i].real * sin(2 * PI*i*j / count_N);
+		}
+	}
 	return 0;
 }
