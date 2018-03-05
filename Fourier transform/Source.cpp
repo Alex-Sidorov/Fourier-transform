9c25c849bb86215f09f9fcf4d23a42beaec9a4ce
Саша Сидоров <Sidorov1999@bk.ru>
2018-03-05 08:05:49 +0300

    reverse

--- Fourier transform/Source.cpp
+++ Fourier transform/Source.cpp
@@ -6,14 +6,23 @@ const double PI = 3.14159265;
 
 struct Complex
 {
-	long double real = 0;
-	long double image = 0;
+	float real = 0;
+	double image = 0;
 };
 
 
 double module_comlex(struct Complex value)
 {
 	return sqrt(value.real*value.real + value.image*value.image);
+}
+
+void zeroing_arr(Complex *arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		arr[i].real = 0;
+		arr[i].image = 0;
+	}
 }
 
 int main()
@@ -46,7 +55,6 @@ int main()
 		std::cin >> x_n[i].image;
 	}
 
-	double summa = 0;
 	int j = sqrt(-1);
 	for (int i = 0; i < count_N; i++)
 	{
@@ -60,8 +68,9 @@ int main()
 			else
 			{
 				X_k[i].real += x_n[j].real * cos(2 * PI*i*j / count_N);
+				X_k[i].real += -1 * x_n[j].real * sin(2 * PI*i*j / count_N);
 				X_k[i].image += x_n[j].image * cos(2 * PI*i*j / count_N);
-				X_k[i].image += -1 * x_n[j].image * sin(2 * PI*i*j / count_N);
+				X_k[i].real += -1 * x_n[j].image * sin(2 * PI*i*j / count_N);
 			}
 		}
 	}
@@ -73,8 +82,45 @@ int main()
 			std::cout <<" + j*"<< X_k[i].image;
 		else if(X_k[i].image < 0)
 			std::cout << " - j*" << -1*X_k[i].image;
-		std::cout <<"   module(X(" << i << ")=" << module_comlex(X_k[i]) << std::endl;
+		std::cout <<"   module(X(" << i << "))=" << module_comlex(X_k[i]) << std::endl;
 	}
+
+	zeroing_arr(x_n, count_N);
+
+	for (int i = 0; i < count_N; i++)
+	{
+		for (int j = 0; j < count_N; j++)
+		{
+			if (X_k[j].image == 0)
+			{
+				x_n[i].real += X_k[j].real * cos(2 * PI*i*j / count_N);
+				x_n[i].image += X_k[j].real * sin(2 * PI*i*j / count_N);
+			}
+			else
+			{
+				x_n[i].real += X_k[j].real * cos(2 * PI*i*j / count_N);
+				x_n[i].image += X_k[j].real * sin(2 * PI*i*j / count_N);
+				x_n[i].image += X_k[j].image * cos(2 * PI*i*j / count_N);
+				x_n[i].real += X_k[j].image * sin(2 * PI*i*j / count_N);
+			}
+		}
+		x_n[i].real /= 3;
+		x_n[i].image /= 3;
+		if (x_n[i].real < 0.000001)x_n[i].real = 0;
+		if (x_n[i].image < 0.000001)x_n[i].image = 0;
+	}
+
+	std::cout << std::endl;
+	for (int i = 0; i < count_N; i++)
+	{
+		std::cout << "x(" << i << ")=" << x_n[i].real;
+		if (x_n[i].image > 0)
+			std::cout << " + j*" << x_n[i].image;
+		else if (x_n[i].image < 0)
+			std::cout << " - j*" << -1 * x_n[i].image;
+		std::cout << "   module(X(" << i << "))=" << module_comlex(x_n[i]) << std::endl;
+	}
+
 	_getch();
 	return 0;
 }
